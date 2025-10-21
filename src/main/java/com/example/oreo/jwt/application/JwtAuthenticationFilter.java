bde7a389ceae4e79bf31d29c7bec3bebbf38db56
AM_Erizur <sajoudks@outlook.com>
2025-10-21 18:29:48 -0500

    commit the changes and jwt

---
+++ src/main/java/com/example/oreo/jwt/application/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.example.oreo.jwt.application;
+
+import com.example.oreo.jwt.domain.JwtService;
+import com.example.oreo.user.domain.UserService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    //private final UserService userService;
+
+    @Override
+    protected void doFilterInternal (HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        String jwtToken = resolveToken(request);
+
+        if (StringUtils.hasText(jwtToken)) {
+            final Optional<Claims> maybeClaims = jwtService.extractAllClaims(jwtToken);
+            final Optional<Authentication> maybeAuth =
+                    maybeClaims.flatMap(claims -> jwtService.getAuthentication(jwtToken, claims));
+
+            maybeAuth.ifPresent(SecurityContextHolder.getContext()::setAuthentication);
+        }
+
+        try {
+        filterChain.doFilter(request, response);
+        } finally {
+            SecurityContextHolder.clearContext();
+        }
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        final String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
