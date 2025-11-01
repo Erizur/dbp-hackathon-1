2030b18104db1001112bdeaaafa018a9aab1e3a9
AM_Erizur <sajoudks@outlook.com>
2025-11-01 16:44:23 -0500

    add to user controller

--- src/main/java/com/example/oreo/user/application/UserController.java
+++ src/main/java/com/example/oreo/user/application/UserController.java
@@ -2,6 +2,10 @@ package com.example.oreo.user.application;
 
 
 import com.example.oreo.user.repository.UserRepository;
+
+import jakarta.annotation.security.RolesAllowed;
+
+import com.example.oreo.user.domain.Role;
 import com.example.oreo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,16 +19,19 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
+    @RolesAllowed("CENTRAL")
     public List<User> all() {
         return userRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @RolesAllowed("CENTRAL")
     public User one(@PathVariable Long id) {
         return userRepository.findById(id).orElseThrow();
     }
 
     @DeleteMapping("/{id}")
+    @RolesAllowed("CENTRAL")
     public void delete(@PathVariable Long id) {
         userRepository.deleteById(id);
     }
